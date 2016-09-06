99dd321d32cc9153767f5c6d1a7098cc1b52b5e2
Benjamin Vedder <benjamin@vedder.se>
2016-09-06 15:52:22 +0200

    Added log c files

---
+++ Embedded/RC_Controller/log.c
@@ -0,0 +1,105 @@
+/*
+	Copyright 2016 Benjamin Vedder	benjamin@vedder.se
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "log.h"
+#include "pos.h"
+#include "commands.h"
+#include "servo_simple.h"
+
+#include <string.h>
+
+// Variables
+bool m_log_en;
+bool m_write_split;
+char m_log_name[LOG_NAME_MAX_LEN + 1];
+
+// Threads
+static THD_WORKING_AREA(log_thread_wa, 2048);
+static THD_FUNCTION(log_thread, arg);
+
+void log_init(void) {
+	m_log_en = false;
+	m_write_split = true;
+	strcpy(m_log_name, "Undefined");
+
+	chThdCreateStatic(log_thread_wa, sizeof(log_thread_wa), NORMALPRIO, log_thread, NULL);
+}
+
+void log_set_enabled(bool enabled) {
+	if (enabled && !m_log_en) {
+		m_write_split = true;
+	}
+
+	m_log_en = enabled;
+}
+
+void log_set_name(char *name) {
+	strcpy(m_log_name, name);
+}
+
+static THD_FUNCTION(log_thread, arg) {
+	(void)arg;
+
+	chRegSetThreadName("Log");
+
+	for(;;) {
+		if (m_log_en) {
+			if (m_write_split) {
+				commands_printf_log_usb("//%s\n", m_log_name);
+				m_write_split = false;
+			}
+
+#ifdef LOG_EN_CARREL
+			mc_values val;
+			POS_STATE pos;
+			float accel[3];
+			float gyro[3];
+			float mag[3];
+
+			pos_get_mc_val(&val);
+			pos_get_pos(&pos);
+			pos_get_imu(accel, gyro, mag);
+
+			commands_printf_log_usb("%.2f %.2f %.2f %.2f %.3f %.3f %.3f %.3f %.3f %.2f %.3f %.3f %.3f %.1f %.1f %.1f %.1f %.1f %.1f %d %.3f\n",
+					(double)val.temp_mos,
+					(double)val.current_in,
+					(double)val.current_motor,
+					(double)val.v_in,
+					(double)pos.px,
+					(double)pos.py,
+					(double)pos.px_gps,
+					(double)pos.py_gps,
+					(double)pos.pz_gps,
+					(double)pos.speed,
+					(double)pos.yaw,
+					(double)accel[0],
+					(double)accel[1],
+					(double)accel[2],
+					(double)gyro[0],
+					(double)gyro[1],
+					(double)gyro[2],
+					(double)mag[0],
+					(double)mag[1],
+					(double)mag[2],
+					val.tachometer,
+					(double)servo_simple_get_pos_now());
+#endif
+		}
+
+		chThdSleepMilliseconds(LOG_INTERVAL_MS);
+	}
+}
---
+++ Embedded/RC_Controller/log.h
@@ -0,0 +1,30 @@
+/*
+	Copyright 2016 Benjamin Vedder	benjamin@vedder.se
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LOG_H_
+#define LOG_H_
+
+#include "ch.h"
+#include "chtypes.h"
+#include "chsystypes.h"
+
+// Functions
+void log_init(void);
+void log_set_enabled(bool enabled);
+void log_set_name(char *name);
+
+#endif /* LOG_H_ */
