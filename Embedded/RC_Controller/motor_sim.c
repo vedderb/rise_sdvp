dcb04575f40a0d2a0e2ca8d102836baf3a0e364c
Benjamin Vedder <benjamin@vedder.se>
2017-12-04 14:27:06 +0100

    Added timeout to motor simulator

--- Embedded/RC_Controller/motor_sim.c
+++ Embedded/RC_Controller/motor_sim.c
@@ -14,12 +14,14 @@
 #define INPUT_VOLTAGE					39.0
 #define ERPM_PER_SEC					25000.0
 #define MAX_CURRENT						80.0
+#define TIMEOUT							2.0
 
 // Private variables
 static bool m_is_running;
 static mc_values m_values;
 static motor_control_mode m_mode;
 static float m_mode_value;
+static float m_timeout;
 
 // Private functions
 static void motor_control_set(motor_control_mode mode, float value);
@@ -33,6 +35,7 @@ void motor_sim_init(void) {
 	m_is_running = false;
 	m_mode = MOTOR_CONTROL_DUTY;
 	m_mode_value = 0.0;
+	m_timeout = 0.0;
 	chThdCreateStatic(sim_thread_wa, sizeof(sim_thread_wa), NORMALPRIO, sim_thread, NULL);
 }
 
@@ -109,6 +112,13 @@ static THD_FUNCTION(sim_thread, arg) {
 			m_values.watt_hours = 0.0;
 			m_values.watt_hours_charged = 0.0;
 			m_values.fault_code = FAULT_CODE_NONE;
+
+			// Timeout
+			m_timeout += dt;
+			if (m_timeout > TIMEOUT) {
+				m_mode = MOTOR_CONTROL_CURRENT_BRAKE;
+				m_mode_value = 10.0;
+			}
 		}
 
 		iteration_timer = chThdSleepUntilWindowed(iteration_timer,
@@ -119,6 +129,7 @@ static THD_FUNCTION(sim_thread, arg) {
 static void motor_control_set(motor_control_mode mode, float value) {
 	m_mode = mode;
 	m_mode_value = value;
+	m_timeout = 0.0;
 }
 
 static void motor_values_requested(void) {
