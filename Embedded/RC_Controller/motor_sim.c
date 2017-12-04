5539b9133f0e2aeb092a0f1928a93582ad31dc40
Benjamin Vedder <benjamin@vedder.se>
2017-12-04 14:07:52 +0100

    Finished motor simulator

--- Embedded/RC_Controller/motor_sim.c
+++ Embedded/RC_Controller/motor_sim.c
@@ -5,12 +5,21 @@
 #include "ch.h"
 #include "hal.h"
 #include "bldc_interface.h"
+#include "utils.h"
 
 // Settings
 #define SIMULATION_TIME_MS				10
+#define MOTOR_KV						520.0
+#define MOTOR_POLES						4.0
+#define INPUT_VOLTAGE					39.0
+#define ERPM_PER_SEC					25000.0
+#define MAX_CURRENT						80.0
 
 // Private variables
 static bool m_is_running;
+static mc_values m_values;
+static motor_control_mode m_mode;
+static float m_mode_value;
 
 // Private functions
 static void motor_control_set(motor_control_mode mode, float value);
@@ -22,6 +31,8 @@ static THD_FUNCTION(sim_thread, arg);
 
 void motor_sim_init(void) {
 	m_is_running = false;
+	m_mode = MOTOR_CONTROL_DUTY;
+	m_mode_value = 0.0;
 	chThdCreateStatic(sim_thread_wa, sizeof(sim_thread_wa), NORMALPRIO, sim_thread, NULL);
 }
 
@@ -45,8 +56,59 @@ static THD_FUNCTION(sim_thread, arg) {
 	systime_t iteration_timer = chVTGetSystemTime();
 
 	for(;;) {
+		float dt = (float)SIMULATION_TIME_MS / 1000.0;
+
 		if (m_is_running) {
-			// TODO!
+			const float rpm_max = INPUT_VOLTAGE * MOTOR_KV * (MOTOR_POLES / 2.0);
+
+			switch (m_mode) {
+			case MOTOR_CONTROL_DUTY: {
+				float rpm = m_mode_value * rpm_max;
+				utils_step_towards(&m_values.rpm, rpm, ERPM_PER_SEC * dt);
+			} break;
+
+			case MOTOR_CONTROL_CURRENT: {
+				utils_step_towards(&m_values.rpm, SIGN(m_mode_value) * rpm_max,
+						ERPM_PER_SEC * dt * (fabsf(m_mode_value) / MAX_CURRENT));
+			} break;
+
+			case MOTOR_CONTROL_CURRENT_BRAKE: {
+				utils_step_towards(&m_values.rpm, 0.0,
+						ERPM_PER_SEC * dt * (fabsf(m_mode_value) / MAX_CURRENT));
+			} break;
+
+			case MOTOR_CONTROL_RPM: {
+				utils_step_towards(&m_values.rpm, m_mode_value, ERPM_PER_SEC * dt);
+			} break;
+
+			case MOTOR_CONTROL_POS: {
+				// TODO
+			} break;
+
+			default:
+				break;
+			}
+
+			// Friction
+			m_values.rpm *= powf(0.9, dt);
+			utils_step_towards(&m_values.rpm, 0.0, ERPM_PER_SEC * dt * 0.02);
+
+			// Update values
+			m_values.tachometer += m_values.rpm / 60.0 * dt * 6.0;
+			m_values.tachometer_abs += fabsf(m_values.rpm) / 60.0 * dt * 6.0;
+			m_values.v_in = INPUT_VOLTAGE;
+			m_values.duty_now = m_values.rpm / rpm_max;
+			m_values.temp_mos = 25.0;
+			m_values.temp_motor = 25.0;
+			m_values.current_motor = 0.0;
+			m_values.current_in = m_values.duty_now * m_values.current_motor;
+			m_values.id = 0.0;
+			m_values.iq = m_values.current_motor;
+			m_values.amp_hours = 0.0;
+			m_values.amp_hours_charged = 0.0;
+			m_values.watt_hours = 0.0;
+			m_values.watt_hours_charged = 0.0;
+			m_values.fault_code = FAULT_CODE_NONE;
 		}
 
 		iteration_timer = chThdSleepUntilWindowed(iteration_timer,
@@ -55,10 +117,10 @@ static THD_FUNCTION(sim_thread, arg) {
 }
 
 static void motor_control_set(motor_control_mode mode, float value) {
-	(void)mode;
-	(void)value;
+	m_mode = mode;
+	m_mode_value = value;
 }
 
 static void motor_values_requested(void) {
-
+	send_values_to_receiver(&m_values);
 }
