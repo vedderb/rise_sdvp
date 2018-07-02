56a142f88d8798a9d7222b82b2099acbf538fb2e
Benjamin Vedder <benjamin@vedder.se>
2018-07-02 12:01:14 +0200

    Changed default tile server to cover more than sweden

--- Linux/RControlStation/mapwidget.cpp
+++ Linux/RControlStation/mapwidget.cpp
@@ -198,9 +198,9 @@ MapWidget::MapWidget(QWidget *parent) : QWidget(parent)
     // Hardcoded for now
     mOsm->setCacheDir("osm_tiles");
     //    mOsm->setTileServerUrl("http://tile.openstreetmap.org");
-    //    mOsm->setTileServerUrl("http://c.osm.rrze.fau.de/osmhd"); // Also https
+    mOsm->setTileServerUrl("http://c.osm.rrze.fau.de/osmhd"); // Also https
     //    mOsm->setTileServerUrl("http://tiles.vedder.se/osm_tiles");
-    mOsm->setTileServerUrl("http://tiles.vedder.se/osm_tiles_hd");
+    //mOsm->setTileServerUrl("http://tiles.vedder.se/osm_tiles_hd");
 
     connect(mOsm, SIGNAL(tileReady(OsmTile)),
             this, SLOT(tileReady(OsmTile)));
