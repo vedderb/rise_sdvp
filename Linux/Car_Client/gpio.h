4cdcb8c923e46d03c0a06a0fb5495744c68c88e5
Jesper Blidkvist <jesper.blidkvist@live.se>
2019-08-08 10:00:02 +0200

    Moved PIN defines and other defines to gpio.cpp

--- Linux/Car_Client/gpio.h
+++ Linux/Car_Client/gpio.h
@@ -1,10 +1,6 @@
 #ifndef GPIO_H
 #define GPIO_H
 
-//TODO Check which are used for other things and remove these.
-#define GPIO_PINS 0, 1, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 23, 24, 25
-#define STR_LENGTH 64
-
 #include <QObject>
 #include <stdio.h>
 #include <string.h>
